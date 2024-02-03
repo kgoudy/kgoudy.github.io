035911f7f332940f34d24c1b12480ad0dd40446e
Koki Goudy <kgoudy64@gmail.com>
2024-02-03 15:15:24 -0600

    installing tailwind

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    Changa: ["Changa"],
+  },
+};
+// eslint-disable-next-line no-unused-labels
+plugins: [require("flowbite/plugin")];
