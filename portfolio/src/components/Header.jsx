458ca298f1f915f34f4ae4d8c36d6907a3e87f1a
Koki Goudy <kgoudy64@gmail.com>
2024-02-08 15:24:03 -0600

    update to navbar

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -7,20 +7,11 @@ function Header() {
     <>
       <nav className="bg-black">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <img src="/fleur.jpg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-yellow-400">
-              Koki D Goudy
-            </span>
-          </a>
+          <img src="/fleur.jpg" className="h-8" alt="Logo" />
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg bg-black md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li>
                 <a
-                  href="#"
                   className="block py-2 px-3 text-yellow-400"
                   aria-current="page"
                 >
