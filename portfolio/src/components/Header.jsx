334d690b8d3e9deaff40147ce4acc3cd41a6e0ab
Koki Goudy <kgoudy64@gmail.com>
2024-02-08 11:47:51 -0600

    update navbar

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -4,25 +4,49 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <nav className="bg-black border-gray-200 dark:bg-gray-900 flex">
-      <div className="flex mt-2">
-        <img src="/fleur.jpg" className="h-12 px-3 float-left py-2 "></img>
-        <h1 className="text-3xl text-yellow-400 ">Koki D Goudy</h1>
-      </div>
-      <div className="flex">
-        <nav className="flex">
-          <li className="text-3xl text-yellow-400 py-2">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="text-3xl text-yellow-400 py-2">
-            <Link to="#">Projects</Link>
-          </li>
-          <li className="text-3xl text-yellow-400 py-2">
-            <Link to="/contact">Contact</Link>
-          </li>
-        </nav>
-      </div>
-    </nav>
+    <>
+      <nav className="bg-black">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img src="/fleur.jpg" className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-yellow-400">
+              Koki D Goudy
+            </span>
+          </a>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg bg-black md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 px-3 text-yellow-400"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-yellow-400">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-yellow-400">
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block py-2 px-3 text-yellow-400">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
