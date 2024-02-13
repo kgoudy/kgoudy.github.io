3a812c753fcfec9d4e773084b644441245ee8bca
Koki Goudy <kgoudy64@gmail.com>
2024-02-13 12:04:01 -0600

    update

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable no-unused-vars */
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Header />
+      <div>
+        <img src="/koki.jpg" alt="Koki" id="koki-img" />
+      </div>
+      <div>
+        <h1 className="text-7xl" id="koki-intro">
+          Koki D Goudy
+        </h1>
+        <p>Junior Software Devloper</p>
+      </div>
+      <Footer />
     </>
   );
 }
